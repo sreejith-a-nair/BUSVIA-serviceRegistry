c61d7568d4e2202c03d763f38ffc44633298072b
sreejith-a-nair <anairsreejith1998@gmail.com>
2024-04-12 23:31:42 +0530

    Initial commit

---
+++ src/test/java/com/Busviaserviceregistry/BusviaServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Busviaserviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusviaServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
